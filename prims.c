f5dca45eac30ff050439b3f6fe6a2b46011b613e
Denis Robert <119649014+Denis-Robert@users.noreply.github.com>
2023-01-18 11:40:16 +0530

    Update prims.c

--- prims.c
+++ prims.c
@@ -1,17 +1,17 @@
 // For the given testcase, Prim's works because the 1st iteration is the optimal one and hence it stops there before running into problems
 int g[5][5]={{0,4,2,0,0},{0,0,3,2,3},{0,1,0,4,5},{0,0,0,0,0},{0,0,0,-5,0}};
 #include<stdio.h>
-int mincost(int edgevalue[],int visit[])
+int min_cost(int edgevalue[],int visit[])
 {
 	int k;
-	int edhukku=100000;
+	int max=100;
 	for(int i=1;i<5;i++)
 	{
 		if(visit[i]==0)
 		{
-			if(edhukku>edgevalue[i])
+			if(max>edgevalue[i])
 			{
-				edhukku=edgevalue[i];
+				max=edgevalue[i];
 				k=i;
 			}
 		}
@@ -21,14 +21,14 @@ int mincost(int edgevalue[],int visit[])
 
 int main()
 {
-	int edgevalue[5],visit[5],munnadi[5];
+	int edgevalue[5],visit[5],prev[5];
 	for(int i=0;i<5;i++)
 	{
 		visit[i]=0;
 		edgevalue[i]=1000;
-		munnadi[i]=0;
+		prev[i]=0;
 	}
-	edgevalue[0]=0; munnadi[0]=0; visit[0]=1;
+	edgevalue[0]=0; prev[0]=0; visit[0]=1;
 	loop:
 	for(int i=0;i<5;i++)
 	{
@@ -37,17 +37,17 @@ int main()
 		if(g[0][i]>0 && g[0][i]<edgevalue[i] && visit[i]==0)
 		{
 			edgevalue[i]=g[0][i];
-			munnadi[i]=s;
+			prev[i]=s;
 		}
 	}
-	s=mincost(edgevalue,visit);
+	int s=min_cost(edgevalue,visit);
 	visit[s-1]=1;
 	for(int i=0;i<5;i++)
 	{
 		if(visit[i]==0)
 			{goto loop;}
 	}
-	printf("vertex\t\tbefore vertex\t\t edgecost");
+	printf("Vertex\t\tPrevious vertex\t\tCost");
 	for(int i=0;i<5;i++)
-		{printf("\5%d\t\t %d\t\t\t %d",i+1,munnadi[i],edgevalue[i]);}
+		{printf("%d\t\t %d\t\t\t %d",i+1,prev[i],edgevalue[i]);}
 }
